bf2b49379590c2e06c256c50e64cd265779bbc6f
柏桐 <11770038+ericbai08@user.noreply.gitee.com>
2025-05-23 00:48:33 +0800

    initialize

---
+++ src/main/java/org/example/motify/MotifyApplication.java
@@ -0,0 +1,13 @@
+package org.example.motify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/motify/MotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.motify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
